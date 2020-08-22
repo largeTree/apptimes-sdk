4abd9bf6b6ade28af0bd44336e31bb25a4f2f0b5
largeTree <qiuxs@qiuxs.com>
2020-08-22 14:50:18 +0800

    增加urlToToken请求

---
+++ src/main/java/com/qiuxs/apptimes/request/UrlToTokenRequest.java
@@ -0,0 +1,97 @@
+package com.qiuxs.apptimes.request;
+
+import com.qiuxs.apptimes.request.anno.ApiField;
+import com.qiuxs.apptimes.request.anno.ApptimesApi;
+import com.qiuxs.apptimes.response.UrlToTokenResponse;
+
+@ApptimesApi("convert/url-to-token")
+public class UrlToTokenRequest extends BaseRequest<UrlToTokenResponse> {
+
+	private static final long serialVersionUID = 3290372182928288943L;
+
+	private String url;
+	private String pid;
+	/**  渠道ID. */
+	@ApiField("relation_id")
+	private Long relationId;
+
+	/**  会员运营ID. */
+	@ApiField("special_id")
+	private Long specialId;
+
+	/**  传给淘宝用来生成会员ID的东西. */
+	@ApiField("external_id")
+	private Long externalId;
+
+	@ApiField("activity_id")
+	private String activityId;
+
+	private String xid;
+	@ApiField("force_uland")
+	private Integer forceUland;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	public Long getRelationId() {
+		return relationId;
+	}
+
+	public void setRelationId(Long relationId) {
+		this.relationId = relationId;
+	}
+
+	public Long getSpecialId() {
+		return specialId;
+	}
+
+	public void setSpecialId(Long specialId) {
+		this.specialId = specialId;
+	}
+
+	public Long getExternalId() {
+		return externalId;
+	}
+
+	public void setExternalId(Long externalId) {
+		this.externalId = externalId;
+	}
+
+	public String getActivityId() {
+		return activityId;
+	}
+
+	public void setActivityId(String activityId) {
+		this.activityId = activityId;
+	}
+
+	public String getXid() {
+		return xid;
+	}
+
+	public void setXid(String xid) {
+		this.xid = xid;
+	}
+
+	public Integer getForceUland() {
+		return forceUland;
+	}
+
+	public void setForceUland(Integer forceUland) {
+		this.forceUland = forceUland;
+	}
+
+}
---
+++ src/main/java/com/qiuxs/apptimes/response/UrlToTokenResponse.java
@@ -0,0 +1,185 @@
+package com.qiuxs.apptimes.response;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class UrlToTokenResponse extends BaseResponse {
+
+	private static final long serialVersionUID = -1193663431994213742L;
+
+	private Data data;
+
+	public Data getData() {
+		return data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public static class Data implements Serializable {
+		private BigDecimal commission;
+		private BigDecimal commissionRate;
+		private BigDecimal currentPrice;
+		private BigDecimal finalCommission;
+		private Long goodId;
+		private String goodPic;
+		private String goodTitle;
+		private Integer isMall;
+		private BigDecimal maxCommissionRate;
+		private BigDecimal minCommissionRate;
+		private String nick;
+		private BigDecimal originPrice;
+		private BigDecimal quanPrice;
+		private Long sales;
+		private String sclickUrl;
+		private Long sellerId;
+		private String token;
+		private String ulandUrl;
+
+		public BigDecimal getCommission() {
+			return commission;
+		}
+
+		public void setCommission(BigDecimal commission) {
+			this.commission = commission;
+		}
+
+		public BigDecimal getCommissionRate() {
+			return commissionRate;
+		}
+
+		public void setCommissionRate(BigDecimal commissionRate) {
+			this.commissionRate = commissionRate;
+		}
+
+		public BigDecimal getCurrentPrice() {
+			return currentPrice;
+		}
+
+		public void setCurrentPrice(BigDecimal currentPrice) {
+			this.currentPrice = currentPrice;
+		}
+
+		public BigDecimal getFinalCommission() {
+			return finalCommission;
+		}
+
+		public void setFinalCommission(BigDecimal finalCommission) {
+			this.finalCommission = finalCommission;
+		}
+
+		public Long getGoodId() {
+			return goodId;
+		}
+
+		public void setGoodId(Long goodId) {
+			this.goodId = goodId;
+		}
+
+		public String getGoodPic() {
+			return goodPic;
+		}
+
+		public void setGoodPic(String goodPic) {
+			this.goodPic = goodPic;
+		}
+
+		public String getGoodTitle() {
+			return goodTitle;
+		}
+
+		public void setGoodTitle(String goodTitle) {
+			this.goodTitle = goodTitle;
+		}
+
+		public Integer getIsMall() {
+			return isMall;
+		}
+
+		public void setIsMall(Integer isMall) {
+			this.isMall = isMall;
+		}
+
+		public BigDecimal getMaxCommissionRate() {
+			return maxCommissionRate;
+		}
+
+		public void setMaxCommissionRate(BigDecimal maxCommissionRate) {
+			this.maxCommissionRate = maxCommissionRate;
+		}
+
+		public BigDecimal getMinCommissionRate() {
+			return minCommissionRate;
+		}
+
+		public void setMinCommissionRate(BigDecimal minCommissionRate) {
+			this.minCommissionRate = minCommissionRate;
+		}
+
+		public String getNick() {
+			return nick;
+		}
+
+		public void setNick(String nick) {
+			this.nick = nick;
+		}
+
+		public BigDecimal getOriginPrice() {
+			return originPrice;
+		}
+
+		public void setOriginPrice(BigDecimal originPrice) {
+			this.originPrice = originPrice;
+		}
+
+		public BigDecimal getQuanPrice() {
+			return quanPrice;
+		}
+
+		public void setQuanPrice(BigDecimal quanPrice) {
+			this.quanPrice = quanPrice;
+		}
+
+		public Long getSales() {
+			return sales;
+		}
+
+		public void setSales(Long sales) {
+			this.sales = sales;
+		}
+
+		public String getSclickUrl() {
+			return sclickUrl;
+		}
+
+		public void setSclickUrl(String sclickUrl) {
+			this.sclickUrl = sclickUrl;
+		}
+
+		public Long getSellerId() {
+			return sellerId;
+		}
+
+		public void setSellerId(Long sellerId) {
+			this.sellerId = sellerId;
+		}
+
+		public String getToken() {
+			return token;
+		}
+
+		public void setToken(String token) {
+			this.token = token;
+		}
+
+		public String getUlandUrl() {
+			return ulandUrl;
+		}
+
+		public void setUlandUrl(String ulandUrl) {
+			this.ulandUrl = ulandUrl;
+		}
+	}
+
+}
