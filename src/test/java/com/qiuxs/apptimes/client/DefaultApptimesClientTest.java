1173593d604501d99c18d990a43260d83a69031a
largeTree <qiuxs@qiuxs.com>
2020-12-12 10:26:15 +0800

    创建京东pid

--- src/test/java/com/qiuxs/apptimes/client/DefaultApptimesClientTest.java
+++ src/test/java/com/qiuxs/apptimes/client/DefaultApptimesClientTest.java
@@ -1,7 +1,12 @@
 package com.qiuxs.apptimes.client;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +14,11 @@ import org.junit.Test;
 import com.qiuxs.apptimes.base.config.UConfigClientConfig;
 import com.qiuxs.apptimes.base.request.UserExpTimeRequest;
 import com.qiuxs.apptimes.base.response.UserExpTimeResponse;
+import com.qiuxs.apptimes.jd.req.PositionCreateRequest;
+import com.qiuxs.apptimes.jd.req.PositionQueryRequest;
+import com.qiuxs.apptimes.jd.resp.PositionQueryResponse;
+import com.qiuxs.apptimes.jd.resp.PositionQueryResponse.Data;
+import com.qiuxs.apptimes.jd.resp.PositionQueryResponse.Result;
 import com.qiuxs.apptimes.tb.request.CouponRequest;
 import com.qiuxs.apptimes.tb.request.OrderListRequest;
 import com.qiuxs.apptimes.tb.request.PublisherTokenGetRequest;
@@ -38,6 +48,70 @@ public class DefaultApptimesClientTest {
 	}
 
 	@Test
+	public void createJdPid() {
+			StringBuilder sb = new StringBuilder();
+			int count = 0;
+			for (int i = 5001; i < 10000; i++) {
+				String spaceName = "JDPID_" + i;
+				sb.append(spaceName).append(",");
+				count++;
+				if (count == 50) {
+					sb.setLength(sb.length() - 1);
+    				PositionCreateRequest pcr = new PositionCreateRequest();
+    				pcr.setKey("e4209eac5db9f8e2e12ac05c0d43b4850bd859b66982adfd8affd5511cd2eb5eb1c0805d854ff5de");
+    				pcr.setType(1);
+    				pcr.setSpaceNameList(sb.toString());
+    				pcr.setUnionType(1);
+    				pcr.setSiteId(3000437209L);
+    				pcr.setUnionId(1000817921L);
+    				this.client.execute(pcr);
+    				count = 0;
+    				sb = new StringBuilder();
+				}
+			}
+		
+	}
+	
+	@Test
+	public void queryPid() {
+		List<Result> all = new ArrayList<>();
+		long total = 0;
+		int page = 0;
+		do {
+			page++;
+    		PositionQueryRequest pqr = new PositionQueryRequest();
+    		pqr.setKey("e4209eac5db9f8e2e12ac05c0d43b4850bd859b66982adfd8affd5511cd2eb5eb1c0805d854ff5de");
+    		pqr.setUnionId(1000817921L);
+    		pqr.setUnionType(3);
+    		pqr.setPageIndex(page);
+    		pqr.setPageSize(100);
+    		PositionQueryResponse resp = this.client.execute(pqr);
+    		Data data = resp.getData();
+    		List<Result> result = data.getResult();
+    		all.addAll(result);
+    		total = data.getTotal();
+		} while(all.size() < total);
+		
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(new File("F:\\Windows\\Users\\qiuxs\\Desktop\\3000436543.sql"));
+			for (Result res : all) {
+				fw.append("INSERT INTO jd_user(id, space_name, pid, site_id, `type`, used_flag, created_time, updated_time) " + 
+						"SELECT MAX(id) + 1, '" + res.getSpaceName() + "','" + res.getId() + "', 3000436543, 1, 0, now(), now() from jd_user;\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	@Test
 	public void textCreateTaoPwd() {
 		TaoPasswordCreateRequest req = new TaoPasswordCreateRequest();
 		req.setPicUrl("https://upcloud.qiuxs.com/users/headimgs/9/2020_03_21_20_42_48");
@@ -46,7 +120,7 @@ public class DefaultApptimesClientTest {
 		TaoPasswordCreateResponse resp = this.client.execute(req);
 		System.out.println(JsonUtils.toJSONString(resp));
 	}
-	
+
 	@Test
 	public void testPublisherTokenGet() {
 		PublisherTokenGetRequest req = new PublisherTokenGetRequest();
@@ -56,7 +130,7 @@ public class DefaultApptimesClientTest {
 		System.out.println(resp.getData().getToken());
 		System.out.println(resp.getData().getUrl());
 	}
-	
+
 	@Test
 	public void testOrderList() {
 		OrderListRequest req = new OrderListRequest();
@@ -72,7 +146,7 @@ public class DefaultApptimesClientTest {
 		System.out.println(JsonUtils.toJSONString(resp));
 		System.out.println(JsonUtils.toJSONString(data));
 	}
-	
+
 	@Test
 	public void testCoupon() {
 		CouponRequest request = new CouponRequest();
@@ -80,7 +154,7 @@ public class DefaultApptimesClientTest {
 		CouponResponse response = this.client.execute(request);
 		System.out.println(JsonUtils.toJSONString(response));
 	}
-	
+
 	@Test
 	public void testUserExpTimeRequest() {
 		UserExpTimeRequest req = new UserExpTimeRequest();
